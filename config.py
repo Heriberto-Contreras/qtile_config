fc747661c3debf71dd39029d106b6616a1b72557
Heriberto-Contreras <119393047+Heriberto-Contreras@users.noreply.github.com>
2024-05-30 22:59:46 -0400

    Update config.py

--- config.py
+++ config.py
@@ -29,6 +29,7 @@ from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 import subprocess
+import json
 
 
 mod = "mod4"
@@ -59,8 +60,9 @@ keys = [
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "space", lazy.spawn("rofi -show drun")),
+    Key([mod, "control"], "equal", lazy.spawn("/home/heriberto/.local/bin/raiseBrightness")),
+    Key([mod, "control"], "minus", lazy.spawn("/home/heriberto/.local/bin/lowerBrightness")),
     # Volume control key binds
-    Key([], "XF86AudioMute", lazy.spawn("amixer -q set Master toggle")),
     Key([mod],
         "equal",
         lazy.spawn("amixer -c 2 sset Master 1+ unmute"),
@@ -71,11 +73,6 @@ keys = [
         lazy.spawn("amixer -c 2 sset Master 1- unmute"),
         lazy.spawn("amixer -c 1 sset Master 1- unmute")
         ),
-    #Key([mod], "equal", lazy.spawn("amixer -c 1 sset Master 1+ unmute")),
-    #Key([mod], "minus", lazy.spawn("amixer -c 1 sset Master 1- unmute")),
-    # Screen Brightness control key binds
-    Key([], "XF86MonBrightnessUp", lazy.spawn("light -A 5")),
-    Key([], "XF86MonBrightnessDown", lazy.spawn("light -U 5"))
 ]
 
 groups = [Group(i) for i in "123456789"]
@@ -119,125 +116,103 @@ layouts = [
     # layout.VerticalTile(),
     # layout.Zoomy(),
 ]
-colors = [
-    ["#002b36", "#002b36"], # background 0
-    ["#839496", "#839496"], # grey 1
-    ["#073642", "#073642"], # different shade of backgroud color 2
-    ["#dc322f", "#dc322f"], # Red 3
-    ["#859900", "#859900"], # Green 4
-    ["#b58900", "#b58900"], # Yellow 5
-    ["#268bd2", "#268bd2"], # Blue 6
-    ["#d33682", "#d33682"], # Magenta 7
-    ["#2aa198", "#2aa198"], # Cyan 8
-    ["#FFFFFF", "#FFFFFF"], # White 9
-    ["#000000", "#000000"], # Black 10
-    ["#6c71c4", "#6c71c4"], # Violet 11
-    ["#cb4b16", "#cb4b16"]  # Orange 12
-]
+
+# Getting pywal colors
+# color0 = background and color15 = foreground
+wal_colors_path = '/home/heriberto/.cache/wal/colors.json'
+with open(wal_colors_path, 'r') as file:
+    data = json.load(file)
+    colors = data['colors'] # this is a colors dictonary 0-15
+
 widget_defaults = dict(
     font="jetbrainsmono",
-    fontsize=12,
+    fontsize=18,
     padding=3,
 )
+
 extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
         bottom=bar.Bar(
             [
-                widget.Sep(
-                    linewidth = 0,
-                    padding = 6,
-                    foreground = colors[2],
-                    background = colors[0]
-                ),
                 widget.GroupBox(
-                    fontsize = 18,
                     margin_y = 4,
                     margin_x = 0,
                     padding_y =10,
                     padding_x = 10,
                     borderwidth = 1,
-                    active = colors[9],
-                    inactive = colors[10],
+                    active = colors['color14'],
+                    inactive = colors['color0'],
                     rounded = False,
                     highlight_method = "block",
-                    this_current_screen_border = colors[12],
-                    this_screen_border = colors[6],
-                    other_current_screen_border = colors[12],
-                    other_screen_border = colors[6],
-                    foreground = colors[2],
-                    background = colors[0],
+                    this_current_screen_border = colors['color1'],
+                    this_screen_border = colors['color4'],
+                    foreground = colors['color15'],
+                    background = colors['color0'],
                 ),
                 widget.WindowName(
-                    background = colors[0],
-                    foreground = colors[9],
-                    fontsize = 18
+                    background = colors['color2'],
+                    foreground = colors['color0'],
                 ),
-                widget.TextBox(
-                    background = colors[0],
-                    foreground = colors[3],
-                    fontsize = 60,
-                    text = "◀",
-                    padding = 0,
+                widget.CPU(
+                    background = colors['color2'],
+                    foreground = colors['color0'],
+                    format = 'CPU: {load_percent}%'
                 ),
-                widget.CheckUpdates(
-                    distro = "Arch",
-                    background = colors[3],
-                    foreground = colors[10],
-                    display_format = "Updates: {updates}",
-                    no_update_string = "No Updates",
-                    fontsize = 18
+                widget.Sep(
+                    background = colors['color2'],
+                    foreground = colors['color0'],
                 ),
-                widget.TextBox(
-                    background = colors[3],
-                    foreground = colors[7],
-                    fontsize = 60,
-                    text = "◀",
-                    padding = 0,
-                ),
-                widget.TextBox(
-                    background = colors[7],
-                    foreground = colors[9],
-                    fontsize = 18,
-                    text = "🔈"
-                ),
-                widget.GenPollText(
-                    update_interval=0.1,
-                    func = lambda: subprocess.check_output("/home/Heriberto/.local/bin/volumeOne").decode("utf-8"),
-                    background = colors[7],
-                    fontsize = 18,
-                ),
-                widget.GenPollText(
-                    update_interval=0.1,
-                    func = lambda: subprocess.check_output("/home/Heriberto/.local/bin/volumeTwo").decode("utf-8"),
-                    background = colors[7],
-                    fontsize = 18,
-                ),
-                widget.TextBox(
-                    background = colors[7],
-                    foreground = colors[5],
-                    fontsize = 60,
-                    text = "◀",
-                    padding = 0,
+                widget.Memory(
+                    background = colors['color2'],
+                    foreground = colors['color0'],
+                    format = 'RAM: {MemPercent:.1f}' '%'
                 ),
                 widget.Systray(
-                    background = colors[5],
-                    fontsize = 18
+                    background = colors['color2'],
+                    foreground = colors['color0'],
                 ),
-                widget.TextBox(
-                    background = colors[5],
-                    foreground = colors[8],
-                    fontsize = 60,
-                    text = "◀",
-                    padding = 0,
+                widget.Sep(
+                    background = colors['color2'],
+                    foreground = colors['color0'],
+                ),
+                widget.Battery(
+                    background = colors['color2'],
+                    foreground = colors['color0'],                   
+                    format = '🔋{percent:2.0%}'
+                ),
+                widget.Sep(
+                    background = colors['color2'],
+                    foreground = colors['color0'],
+                ),
+                widget.GenPollText(
+                    background = colors['color2'],
+                    foreground = colors['color0'],
+                    update_interval = 0.1,
+                    func = lambda: subprocess.check_output("/home/heriberto/.local/bin/getVolume").decode("utf-8"),
+                    fmt = '🔊' '{}'
+                ),
+                widget.Sep(
+                    background = colors['color2'],
+                    foreground = colors['color0'],
+                ),
+                widget.GenPollText(
+                    background = colors['color2'],
+                    foreground = colors['color0'],
+                    update_interval = 0.1,
+                    func = lambda: subprocess.check_output("/home/heriberto/.local/bin/getBrightness").decode("utf-8"),
+                    fmt = '🔆' '{}'
+                ),
+                widget.Sep(
+                    background = colors['color2'],
+                    foreground = colors['color0'],
                 ),
                 widget.Clock(
-                    format="%a %m-%d-%Y %I:%m %p",
-                    fontsize = 18,
-                    background = colors[8],
-                    foreground = colors[10]
-                ),
+                    background = colors['color2'],
+                    foreground = colors['color0'],
+                    format="%Y-%m-%d %a %I:%M %p"
+                )
             ],
             24,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
@@ -282,4 +257,6 @@ auto_minimize = True
 # When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
 wmname = "LG3D"
+
+
 
